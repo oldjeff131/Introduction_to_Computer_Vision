76715925e97020f399fe4bfd84d2252e5d777d6e
oldjeff131 <101303979+oldjeff131@users.noreply.github.com>
2023-10-19 15:22:55 +0800

    Add files via upload

---
+++ Histogram Equalization/4a830212/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace _4a830212
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(292, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "載入和灰階化";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(373, 270);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "直方圖等化";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 332);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "4a830212";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Histogram Equalization/4a830212/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4a830212
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image image;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                image = Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox1.Image = image;
+            }
+            try
+            {
+                int Height = this.pictureBox1.Image.Height;
+                int width = this.pictureBox1.Image.Width;
+                Bitmap NBitmap = new Bitmap(width, Height);
+                Bitmap OBitmap = (Bitmap)this.pictureBox1.Image;
+                Color pixel;
+                for (int x = 0; x < width; x++)
+                {
+                    for (int y = 0; y < Height; y++)
+                    {
+                        pixel = OBitmap.GetPixel(x, y);
+                        int r, g, b, YIQ;
+                        r = pixel.R;
+                        g = pixel.G;
+                        b = pixel.B;
+                        YIQ = (int)(0.299 * r + 0.587 * g + 0.114 * b);
+                        NBitmap.SetPixel(x, y, Color.FromArgb(YIQ, YIQ, YIQ));
+                    }
+                }
+                this.pictureBox1.Image = NBitmap;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "訊息提示");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int Height = this.pictureBox1.Image.Height;
+            int Width = this.pictureBox1.Image.Width;
+            Bitmap Histogrambitmap = new Bitmap(Width, Height);
+            Bitmap oldbitmap = (Bitmap)this.pictureBox1.Image;
+            double[] numa = new double[256];
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Color pixelRGB = oldbitmap.GetPixel(j, i);
+                    if (pixelRGB.R == pixelRGB.G && pixelRGB.R == pixelRGB.B)
+                    {
+                        int grayNumb = pixelRGB.R;
+                        numa[grayNumb]++;
+                    }
+                }
+            }
+            for (int k = 0; k < 256; k++)
+            {
+                double value = numa[k];
+                double rate = value / (Height * Width * 1.0);
+                numa[k] = rate;
+            }
+            for (int j = 0; j < Height; j++)
+            {
+                for (int i = 0; i < Width; i++)
+                {
+                    double DSSum = 0;
+                    Color value = oldbitmap.GetPixel(i, j);
+                    for (int k = 0; k <= value.R; k++)
+                    {    
+                        DSSum += numa[k];
+                    }
+                    byte s = (byte)Math.Round(255 * DSSum);
+                    Color newValue = Color.FromArgb(s, s, s);
+                    Histogrambitmap.SetPixel(i, j, newValue);
+                }
+            }
+            this.pictureBox2.Image = Histogrambitmap;
+        }
+    }
+}
